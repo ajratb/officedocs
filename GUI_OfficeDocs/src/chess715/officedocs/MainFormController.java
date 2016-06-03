5af1428084f10dee40921c5eea8aa76f0de51c49
ayrat <ayrat@nok2-test1.npopm.ru>
2016-06-03 20:37:35 +0700

    the simplest working app

--- GUI_OfficeDocs/src/chess715/officedocs/MainFormController.java
+++ GUI_OfficeDocs/src/chess715/officedocs/MainFormController.java
@@ -5,15 +5,21 @@
  */
 package chess715.officedocs;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.PatternSyntaxException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -24,6 +30,21 @@ import javafx.scene.control.TitledPane;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.stage.DirectoryChooser;
+import org.apache.commons.io.FileUtils;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.demo.IndexFiles;
+
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 
 /**
  *
@@ -52,11 +73,16 @@ public class MainFormController implements Initializable {
     @FXML
     private Label lbPath;
 
+    @FXML
+    private Label lbSearchWarning;
+
     private String path = "";
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         accordion.setExpandedPane(titledPanePath);
+        titledPaneSearch.setCollapsible(false);
+        lbSearchWarning.setText("");
     }
 
     @FXML
@@ -69,10 +95,14 @@ public class MainFormController implements Initializable {
         }
         try {
             path = dirChooser.showDialog(lbPath.getScene().getWindow()).getPath();
-            lbPath.setText(path);
-            boolean structured=structureDocs(path);
-            if(structured)indexDocs();
-            
+            lbPath.setText(path + "\n not indexed yet");
+            boolean structured = structureDocs(path);
+            if (structured) {
+                String indexPath = path + "\\Index";
+                
+                indexIt();
+            }
+            titledPaneSearch.setCollapsible(true);
         } catch (NullPointerException e) {
         }
 
@@ -80,27 +110,159 @@ public class MainFormController implements Initializable {
 
     @FXML
     void handleBtnSearch(ActionEvent event) {
-        SearchResultForm dialog = new SearchResultForm(btnSearch.getParent(),null);
-        dialog.showAndWait();
+        searchIt();
     }
-    
+
     private boolean structureDocs(String path) {
         //1. если нет папки "StructuredDocs", то создаём
-        Path dir=Paths.get(path);
-        try(DirectoryStream<Path> stream=Files.newDirectoryStream(dir, "*.pdf")){
-            for(Path file:stream){
+        Path dir = Paths.get(path);
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.{pdf,ppt,docx,pptx}")) {
+            for (Path file : stream) {
                 System.out.println(file.getFileName());
             }
-        }catch(PatternSyntaxException|DirectoryIteratorException|IOException ex){
+            return true;
+        } catch (PatternSyntaxException | DirectoryIteratorException | IOException ex) {
             System.err.println(ex);
+            return false;
         }
         //2. каждый файл из выбранной папки прогоняем через тику
-        return false;
+
     }
 
-    private void indexDocs() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void indexIt() {
+        String indexPath = path + "\\Index";
+        try {
+                    FileUtils.deleteDirectory(new File(indexPath));
+                    
+                } catch (IOException ex) {
+                    Logger.getLogger(MainFormController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        boolean create = true;
+
+        final Path docDir = Paths.get(path);
+        if (!Files.isReadable(docDir)) {
+            System.out.println("Document directory '" + docDir.toAbsolutePath() + "' does not exist or is not readable, please check the path");
+            System.exit(1);
+        }
+
+        Date start = new Date();
+        try {
+            System.out.println("Indexing to directory '" + indexPath + "'...");
+
+            Directory dir = FSDirectory.open(Paths.get(indexPath));
+            Analyzer analyzer = new StandardAnalyzer();
+            IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+
+            if (create) {
+                // Create a new index in the directory, removing any
+                // previously indexed documents:
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+            } else {
+                // Add new documents to an existing index:
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+            }
+
+            // Optional: for better indexing performance, if you
+            // are indexing many documents, increase the RAM
+            // buffer.  But if you do this, increase the max heap
+            // size to the JVM (eg add -Xmx512m or -Xmx1g):
+            //
+            // iwc.setRAMBufferSizeMB(256.0);
+            IndexWriter writer = new IndexWriter(dir, iwc);
+            IndexFiles.indexDocs(writer, docDir);
+
+            // NOTE: if you want to maximize search performance,
+            // you can optionally call forceMerge here.  This can be
+            // a terribly costly operation, so generally it's only
+            // worth it when your index is relatively static (ie
+            // you're done adding documents to it):
+            //
+            // writer.forceMerge(1);
+            writer.close();
+
+            Date end = new Date();
+            System.out.println(end.getTime() - start.getTime() + " total milliseconds");
+
+        } catch (IOException e) {
+            System.out.println(" caught a " + e.getClass()
+                    + "\n with message: " + e.getMessage());
+        }
+
+        lbPath.setText(path + "\n indexed");
     }
 
-    
+    private void searchIt() {
+        String index = path + "\\Index";
+        String field = "contents";
+
+        String queryString = null;
+        int repeat = 0;
+        boolean raw = false;//if true, uncomment in doPagingSearch method
+
+        int hitsPerPage = 20;
+
+        IndexReader reader;
+        try {
+            reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
+//Reader reader = new InputStreamReader(new FileInputStream(path), "UTF-8");
+            IndexSearcher searcher = new IndexSearcher(reader);
+            Analyzer analyzer = new StandardAnalyzer();
+
+            BufferedReader in = null;
+
+            in = new BufferedReader(new InputStreamReader(System.in, Charset.forName("Windows-1251")));
+            QueryParser parser = new QueryParser(field, analyzer);
+            while (true) {
+                
+                if (txtSearch.getText().equals("")) {
+                    lbSearchWarning.setText("Введите строку поиска");
+                }
+
+                String line = queryString != null ? queryString : txtSearch.getText();
+
+                if (line == null || line.length() == -1) {
+                    break;
+                }
+
+                line = line.trim();
+                if (line.length() == 0) {
+                    break;
+                }
+
+                Query query;
+                try {
+                    query = parser.parse(line);
+
+                    System.out.println("Searching for: " + query.toString(field));
+
+                    if (repeat > 0) {                           // repeat & time as benchmark
+                        Date start = new Date();
+                        for (int i = 0; i < repeat; i++) {
+                            searcher.search(query, 100);
+                        }
+                        Date end = new Date();
+                        System.out.println("Time: " + (end.getTime() - start.getTime()) + "ms");
+                    }
+
+                    SearchResultForm dialog = new SearchResultForm(btnSearch.getParent(), null);
+
+                    dialog.doPagingSearch(in, searcher, query, hitsPerPage,
+                            raw, true);
+                    
+                    txtSearch.setText("");
+
+//                    if (queryString != null) {
+                        break;
+//                    }
+                } catch (ParseException ex) {
+                    Logger.getLogger(MainFormController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            reader.close();
+        } catch (IOException ex) {
+            Logger.getLogger(MainFormController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+//        dialog.showAndWait();
+    }
+
 }
--- GUI_OfficeDocs/src/chess715/officedocs/SearchResultForm.java
+++ GUI_OfficeDocs/src/chess715/officedocs/SearchResultForm.java
@@ -5,30 +5,52 @@
  */
 package chess715.officedocs;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javax.swing.event.HyperlinkEvent;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 
 /**
  *
  * @author ayrat
  */
-public class SearchResultForm extends Stage implements Initializable{
-    
+public class SearchResultForm extends Stage implements Initializable {
+
     @FXML
     private TextArea textArea;
-    
-    public SearchResultForm(Parent parent, List<SearchResult> results) {
+    @FXML
+    private Button btnPrevPage;
 
+    @FXML
+    private Button btnNextPage;
+
+    @FXML
+    private Button btnMorePages;
+
+//    private int start;
+//
+//    private int hitsPerPage;
+
+    public SearchResultForm(Parent parent, List<SearchResult> results) {
+//        start = 0;
         setResizable(false);
 
         initModality(Modality.APPLICATION_MODAL);
@@ -43,10 +65,131 @@ public class SearchResultForm extends Stage implements Initializable{
         this.setTitle("Результаты поиска");
     }
 
-    
+    /**
+     * This demonstrates a typical paging search scenario, where the search
+     * engine presents pages of size n to the user. The user can then go to the
+     * next page if interested in the next hits.
+     *
+     * When the query is executed for the first time, then only enough results
+     * are collected to fill 5 result pages. If the user wants to page beyond
+     * this limit, then the query is executed another time and all hits are
+     * collected.
+     *
+     */
+    public void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query,
+            int hitsPerPage, boolean raw, boolean interactive) throws IOException {
+
+//        this.hitsPerPage = hitsPerPage;
+        int start=0;
+        // Collect enough docs to show 5 pages
+        TopDocs results = searcher.search(query, 5 * hitsPerPage);
+        ScoreDoc[] hits = results.scoreDocs;
+
+        int numTotalHits = results.totalHits;
+        textArea.setText(numTotalHits + " total matching documents \n");
+        
+        int end = Math.min(numTotalHits, hitsPerPage);
+
+        while (true) {
+            //если результатов несколько страниц, то просто активна кнопка "Ещё"
+//            if (end > hits.length) {
+//                textArea.setText("Only results 1 - " + hits.length + " of "
+//                        + numTotalHits + " total matching documents collected. \n");
+//                //textArea.setText("Collect more (y/n) ?");
+//                btnMorePages.setDisable(false);
+//
+//                hits = searcher.search(query, numTotalHits).scoreDocs;
+//            }
+
+            end = Math.min(hits.length, start + hitsPerPage);
+
+            for (int i = start; i < end; i++) {
+//                if (raw) {                              // output raw format
+//                    textArea.setText("doc=" + hits[i].doc + " score=" + hits[i].score);
+//                    continue;
+//                }
+
+                Document doc = searcher.doc(hits[i].doc);
+                String path = doc.get("path");
+                if (path != null) {
+                    textArea.appendText((i + 1) + ". " + path + "\n");
+                    String title = doc.get("title");
+                    if (title != null) {
+                        textArea.appendText("   Title: " + doc.get("title") + "\n");
+                    }
+                } else {
+                    textArea.appendText((i + 1) + ". " + "No path for this document" + "\n");
+                }
+
+            }
+
+            if (!interactive || end == 0) {
+                break;
+            }
+
+            if (numTotalHits >= end) {
+               
+
+//                    System.out.print("Press ");
+                if (start - hitsPerPage >= 0) {
+                    btnPrevPage.setDisable(false);
+                }
+                if (start + hitsPerPage < numTotalHits) {
+                    btnNextPage.setDisable(false);
+                }
+//                    System.out.println("(q)uit or enter number to jump to a page.");
+
+//                    String line = in.readLine();
+//                    if (line.length() == 0 || line.charAt(0) == 'q') {
+//                        quit = true;
+//                        break;
+//                    }
+                //Переход на страницу
+//                        int page = Integer.parseInt(line);
+//                        if ((page - 1) * hitsPerPage < numTotalHits) {
+//                            start = (page - 1) * hitsPerPage;
+//                            break;
+//                        } else {
+//                            System.out.println("No such page");
+//                        }
+            }
+
+            break;
+        }
+
+        end = Math.min(numTotalHits, start + hitsPerPage);
+        showAndWait();
+    }
+
+
+//        btnNextPage.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+//            @Override
+//        public void handle(ActionEvent event) {
+//                if (start + hitsPerPage < numTotalHits) {
+//                            start += hitsPerPage;
+//                        }
+//            }
+//        });
+//
+//        btnPrevPage.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+//            @Override
+//        public void handle(ActionEvent event) {
+//                start = Math.max(0, start - hitsPerPage);
+//            }
+//        });
+//
+//        btnMorePages.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+//            @Override
+//        public void handle(ActionEvent event) {
+//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//            }
+//        });
+
+        
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+        public void initialize(URL location, ResourceBundle resources) {
         //
     }
-    
+
 }
