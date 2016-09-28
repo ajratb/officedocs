31303720b469dcd2a12278536d8a039dda7b3781
ayrat <ayrat@nok2-test1.npopm.ru>
2016-09-28 14:35:50 +0700

    changed

--- Lucen Demo/src/org/apache/lucene/demo/SimplifiedSearchFiles.java
+++ Lucen Demo/src/org/apache/lucene/demo/SimplifiedSearchFiles.java
@@ -1,28 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.lucene.demo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Date;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -39,32 +21,31 @@ import org.apache.lucene.store.FSDirectory;
 /**
  * Simple command-line based search demo.
  */
-public class SearchFiles {
+public class SimplifiedSearchFiles {
 
-    private SearchFiles() {
+    private SimplifiedSearchFiles() {
     }
 
     /**
-     * Simple command-line based search demo.
+     * Simple command-line based search demo. Without "queries", "query" and
+     * "repeat" options
      */
     public static void main(String[] args) throws Exception {
-        
+
         String usage
-                = "Usage:\tjava org.apache.lucene.demo.SearchFiles [-index dir] [-field f] "
-                + "[-repeat n] [-queries file] [-query string] [-raw] "
+                = "Usage:\tjava org.apache.lucene.demo.SearchFiles [-index dir]"
+                + " [-field f] [-query string] [-raw] "
                 + "[-paging hitsPerPage]\n\nSee "
                 + "http://lucene.apache.org/core/4_1_0/demo/ for details.";
-        if (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0]))) {
+        if (args.length > 0 && ("-h".equals(args[0])
+                || "-help".equals(args[0]))) {
             System.out.println(usage);
             System.exit(0);
         }
 
         String index = "index";
         String field = "contents";
-        String queries = null;//"queries.txt"; - файл с запросом. Сработает только один цикл и выход
-        int repeat = 0;
         boolean raw = false;
-        String queryString = null;
         int hitsPerPage = 10;
 
         for (int i = 0; i < args.length; i++) {
@@ -74,17 +55,6 @@ public class SearchFiles {
                 i++;
             } else if ("-field".equals(args[i])) {
                 field = args[i + 1];
-                i++;
-            } //
-            else if ("-queries".equals(args[i])) {
-                queries = args[i + 1];
-                i++;
-            } //
-            else if ("-query".equals(args[i])) {
-                queryString = args[i + 1];
-                i++;
-            } else if ("-repeat".equals(args[i])) {
-                repeat = Integer.parseInt(args[i + 1]);
                 i++;
             } else if ("-raw".equals(args[i])) {
                 raw = true;
@@ -102,25 +72,21 @@ public class SearchFiles {
         IndexSearcher searcher = new IndexSearcher(reader);
         Analyzer analyzer = new StandardAnalyzer();
 
-        BufferedReader in = null;
-        if (queries != null) {           
-            in = Files.newBufferedReader(Paths.get(queries), StandardCharsets.UTF_8);
-        } else {
-            in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-        }
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+
         QueryParser parser = new QueryParser(field, analyzer);
         while (true) {
-            if (queries == null && queryString == null) {                        // prompt the user
-                System.out.println("Enter query: ");
-            }
 
-            String line = queryString != null ? queryString : in.readLine();
+// prompt the user
+            System.out.println("Enter query: ");
+
+            String line = in.readLine();
 
             //Похоже только так и можно выйти
             if (line == null || line.length() == -1) {
                 break;
             }
-
+//если одни пробелы - тоже выходим
             line = line.trim();
             if (line.length() == 0) {
                 break;
@@ -129,20 +95,8 @@ public class SearchFiles {
             Query query = parser.parse(line);
             System.out.println("Searching for: " + query.toString(field));
 
-            if (repeat > 0) {                           // repeat & time as benchmark
-                Date start = new Date();
-                for (int i = 0; i < repeat; i++) {
-                    searcher.search(query, 100);
-                }
-                Date end = new Date();
-                System.out.println("Time: " + (end.getTime() - start.getTime()) + "ms");
-            }
+            doPagingSearch(in, searcher, query, hitsPerPage, raw, true);
 
-            doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
-
-            if (queryString != null) {
-                break;
-            }
         }
         reader.close();
     }
@@ -210,6 +164,8 @@ public class SearchFiles {
             }
 
             if (numTotalHits >= end) {
+                System.out.println("numTotalHits" + numTotalHits);
+                System.out.println("end" + end);
                 boolean quit = false;
                 while (true) {
                     System.out.print("Press ");
