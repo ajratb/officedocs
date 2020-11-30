1fc3d9e299cf7b9c96736f58256a9f90ca1f9df4
ayrat <ayrat715@iss-reshetnev.ru>
2020-11-30 14:36:06 +0700

    upgrade AutoScaledImage

--- pdf/pdf-generator/src/main/java/ru/wts/AutoScaledImage.java
+++ pdf/pdf-generator/src/main/java/ru/wts/AutoScaledImage.java
@@ -19,36 +19,53 @@ public class AutoScaledImage {
     private float timesWider = 1;
     private float timesTaller = 1;
 
+//    Logger appLog = AppLoggerHolder.get();
+
     public AutoScaledImage(Image img) {
+
         this.img = img;
         img.scaleToFit(A4_WIDTH, A4_HEIGHT);
-        if (imgWider()) {
+
+        if (imgWiderThanA4()) {
             timesWider = img.getWidth() / A4_WIDTH;
+//            appLog.debug("in {} times wider", timesWider);
             System.out.println("in " + timesWider + " times wider");
         }
-        if (imgTaller()) {
+
+        if (imgTallerThanA4()) {
             timesTaller = img.getHeight() / A4_HEIGHT;
-            System.out.println("in " + timesTaller + " times taller");
+//            appLog.debug("in {} times taller", timesTaller);
         }
 
         if (timesWider - timesTaller > 0.001) {
-            System.out.println("WIDER!");
+//            appLog.debug("- WIDER!: calculate Y higher position");
             startPos[1] = calculateYHigherPos();//calculateYPos();
         } else if (timesTaller - timesWider > 0.001) {
-            System.out.println("TALLER!");
+//            appLog.debug("- TALLER!: calculate X position");
             startPos[0] = calculateXPos();
         } else {
-            if (timesTaller > 1 || timesWider > 1) {
-                img.setAbsolutePosition(startPos[0], startPos[1]);
+//            appLog.debug("- PROPORTIONAL IMG OR SMALLER: ");
+            if (timesTaller > 1 || timesWider > 1) {//PROPORTIONAL
+//                appLog.debug("-- set ABS.POSITION: {}, {}", startPos[0], startPos[1]);
+                img.setAbsolutePosition(startPos[0], startPos[1]);//1, 1
+                return;
             }
-            img.setAlignment(Image.MIDDLE);
-            return;
+            final double A4_PROPORTION = A4_WIDTH / A4_HEIGHT;
+            final double IMG_PROPORTION = img.getWidth() / img.getHeight();
+            
+//            appLog.debug("- {} : {}", A4_PROPORTION, IMG_PROPORTION);
+            
+            if (img.getWidth() - img.getHeight() > -100) {
+//                appLog.debug("-- set MIDDLE alignment");
+                img.setAlignment(Image.MIDDLE);
+                return;
+            }
+            
         }
         img.setAbsolutePosition(startPos[0], startPos[1]);
-
     }
 
-    private boolean imgWider() {
+    private boolean imgWiderThanA4() {
         float w = img.getWidth();
         if (Math.abs(w - A4_WIDTH) > 0.5) {
             return w > A4_WIDTH;
@@ -56,24 +73,18 @@ public class AutoScaledImage {
         return false;
     }
 
-    private float calculateYPos() {
-        float times = img.getWidth() / A4_WIDTH;
-        float heightAfter = img.getHeight() / times;
-        return (A4_HEIGHT - heightAfter) / 2;
+    private boolean imgTallerThanA4() {
+        float h = img.getHeight();
+        if (Math.abs(h - A4_HEIGHT) > 0.5) {
+            return h > A4_HEIGHT;
+        }
+        return false;
     }
 
     private float calculateYHigherPos() {
         float times = img.getWidth() / A4_WIDTH;
         float heightAfter = img.getHeight() / times;
         return (A4_HEIGHT - heightAfter);
-    }
-
-    private boolean imgTaller() {
-        float h = img.getHeight();
-        if (Math.abs(h - A4_HEIGHT) > 0.5) {
-            return h > A4_HEIGHT;
-        }
-        return false;
     }
 
     private float calculateXPos() {
@@ -85,4 +96,10 @@ public class AutoScaledImage {
     public Image getImage() {
         return img;
     }
+
+//    private float calculateYPos() {
+//        float times = img.getWidth() / A4_WIDTH;
+//        float heightAfter = img.getHeight() / times;
+//        return (A4_HEIGHT - heightAfter) / 2;
+//    }
 }
